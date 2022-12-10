25ec67753c9461afcec24ddddce07a1ae8ee5d9c
biruk Kalewold <birukkalewoldasrat@gmail.com>
2022-12-10 15:15:21 +0300

    added toggling moblile menu on small screens

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const hamburgerbuttenElement = document.querySelector("#hamburger-icon");
+const mobileMenuElement = document.querySelector("#mobile-menu");
+
+hamburgerbuttenElement.addEventListener("click", function () {
+  hamburgerbuttenElement.classList.toggle("open");
+  mobileMenuElement.classList.toggle("hidden");
+});
